834ab9526d80c7163d79e05d731d579eb376a6a6
Ibrahim Moussa <i.moussa.15.im@gmail.com>
2024-10-26 22:52:32 +0300

    add my python codes

--- assignment 4-Ibrahim-Moussa-FCS.py
+++ assignment 4-Ibrahim-Moussa-FCS.py
@@ -1,9 +1,13 @@
-
-given=input("enter ur word: ")
 word = []
 
-word.append(given)
-elemnt=word.pop
+given = input("Enter your word: ").lower()
 
-if given == elemnt:
-    print("diagram")
+for element in given:
+    word.append(element)
+
+reversed_word = ''.join(word.pop() for i in range(len(word)))
+
+if given == reversed_word:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
