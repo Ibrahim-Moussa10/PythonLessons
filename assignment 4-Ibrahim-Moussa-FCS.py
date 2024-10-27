a10a973db5e8a0652d7c626657a521fc5f8fb14b
Ibrahim Moussa <i.moussa.15.im@gmail.com>
2024-10-27 12:53:04 +0200

    finishing assignment 04

--- assignment 4-Ibrahim-Moussa-FCS.py
+++ assignment 4-Ibrahim-Moussa-FCS.py
@@ -34,21 +34,109 @@
 
 #Ex : 3
 
-def decode_messages(message):
-    stack = []
+# def decode_message(message):
+#     stack = []
+#     current_word = ""
+    
+#     for char in message:
+#         if char == '*':
+#             if current_word:
+#                 stack.append(current_word)
+#                 current_word = ""
+#         else:
+#             current_word += char
+            
+#     if current_word:
+#         stack.append(current_word)
 
-    for element in message:
-        if element in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ':
-            stack.append(element)
-        elif element == '*':
-            if stack:
-                stack.pop()
+#     return ' '.join(reversed(stack))
 
-    while stack:
-        stack.pop()
+# message = "SIVLE ****** DAED TNSI ***"
+# decoded = decode_message(message[::-1])
+# print(decoded)
 
-    return ''.join(stack).strip()
+#Ex : 4
+class Node:
+    def __init__(self, info, n=None):
+        self.info = info
+        self.next = n
 
-input = "SIVLE ****** DAED TNSI ***"
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
 
-print(decode_messages(input)) 
+    def addToHead(self, val):
+        if self.size == 0:
+            n = Node(val)
+            self.head = n
+            self.tail = n
+        else:
+            n = Node(val, self.head)
+            self.head = n
+        self.size += 1
+
+    def addToTail(self, val):
+        if self.size == 0:
+            n = Node(val)
+            self.head = n
+            self.tail = n
+        else:
+            n = Node(val)
+            self.tail.next = n
+            self.tail = n
+        self.size += 1
+
+    def deleteAtLocation(self, location):
+        if self.size==0:
+            return None
+
+        if location == 0:
+            self.head = self.head.next
+            self.size -= 1
+            if self.size == 0:
+                self.tail = None
+            return
+
+        current = self.head
+        previous = None
+        count = 0
+
+        while current and count < location:
+            previous = current
+            current = current.next
+            count += 1
+
+        if current is None:
+            return None
+
+        previous.next = current.next
+        if previous.next is None:
+            self.tail = previous
+        self.size -= 1
+
+    def printLL(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.info, end=" -> ")
+            temp = temp.next
+        print("None")
+
+ll = LinkedList()
+ll.addToHead(12)
+ll.addToHead(56)
+ll.addToHead(76)
+ll.addToHead(11)
+ll.addToHead(0)
+
+ll.printLL()
+
+ll.deleteAtLocation(0)
+ll.printLL()
+
+ll.deleteAtLocation(2)
+ll.printLL()
+
+ll.deleteAtLocation(0)
+ll.printLL()
